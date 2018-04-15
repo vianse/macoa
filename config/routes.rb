90dfd899c3cf49957ce36406677e67f1e3a1c35b
Juan Carlos Santiago <krlos013@gmail.com>
2018-04-15 16:07:45 -0500

    cambio Base de Datos Produccion

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  resources :salas
-  resources :tipo_venta
-  resources :step_threes
-  resources :step_fours
-  resources :step_fives
-  resources :step_nines
-  resources :step_eights
-  resources :step_sevens
-  resources :step_sixes
-  resources :step_twos
-  resources :delivery_flows
-  resources :flows
+
   root "delivery_flows#index"
   constraints subdomain: /^(?!www)(\w+)/ do
     resources :projects
@@ -29,9 +18,22 @@ Rails.application.routes.draw do
         resources :flow, path: "flow"
         resources :first_block, path: "first_block"
         resources :add_block, path: "add_block"
+        resources :salas
+        resources :tipo_venta
+        resources :step_threes
+        resources :step_fours
+        resources :step_fives
+        resources :step_nines
+        resources :step_eights
+        resources :step_sevens
+        resources :step_sixes
+        resources :step_twos
+        resources :delivery_flows
+
       end
     end
   end
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
